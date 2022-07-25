debdda8cc4ad0104c26a2727720b4977b2e35b24
Vardges Musakhanyan <danoville@gmail.com>
2022-07-25 21:33:20 +0400

    Add project files.

---
+++ linkedlist/Node.cs
@@ -0,0 +1,81 @@
+﻿namespace linkedlist
+{
+    internal class Node<T>
+    {
+        public Node<T>? Next { get; set; }
+
+        public T Value { get; set; }
+        public Node(Node<T> next, T value)
+        {
+            Next = next;
+            Value = value;
+        }
+
+    }
+
+    class LinkedList<T>
+    {
+        private Node<T>? First { get; set; }
+        private Node<T>? Last { get; set; }
+
+        public LinkedList()
+        {
+            First = null;
+            Last = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return First == null && Last == null;
+        }
+
+        public void Add(T value)
+        {
+            if (IsEmpty())
+            {
+                First = new Node<T>(null, value);
+                Last = First;
+            }
+            else
+            {
+                Last.Next = new Node<T>(null, value);
+                Last = Last.Next;
+            }
+        }
+
+        public int GetSize()
+        {
+            int i = 0;
+            for (Node<T> node = First; node != Last.Next; node = node.Next)
+            {
+                i++;
+            }
+            return i;
+        }
+
+        public T Get(int index)
+        {
+            if (IsEmpty())
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int i = 0;
+            for (Node<T> node = First; node != Last.Next; node = node.Next)
+            {
+                if (i == index)
+                {
+                    return node.Value;
+                }
+                else if (node == Last.Next)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                i++;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+
+
+    }
+}
---
+++ linkedlist/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace linkedlist
+{
+    class Program
+    {
+
+        static public void Main(String[] args)
+        {
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            Console.WriteLine(linkedList.Get(2));
+        }
+    }
+}
+
